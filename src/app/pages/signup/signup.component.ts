e11e7976b2a233da6c5063afa5b4f1a6f5a5febc
CHOTALIYA SAHIL BHUPATBHAI <51098526+sahilchotaliya@users.noreply.github.com>
2024-08-05 13:18:44 +0530

    solve error in phone to number

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from './../../services/user.service';
 import Swal from 'sweetalert2';
@@ -15,25 +16,27 @@ export class SignupComponent implements OnInit {
     password: '',
     lastname: '',
     email: '',
-    phone: ''
+    number: ''
   };
 
-  constructor(private userService: UserService, private snackBar: MatSnackBar) {}
+  constructor(private userService: UserService, private snackBar: MatSnackBar, private router: Router) {} // Inject Router
 
   ngOnInit(): void {
     // Initialization logic if needed
   }
 
   formSubmit(): void {
-    if (!this.user.username) {
-      this.snackBar.open('Username is required!', 'OK', { duration: 3000 });
+    if (!this.user.username || !this.user.password || !this.user.email || !this.user.firstname || !this.user.lastname || !this.user.number) {
+      this.snackBar.open('All fields are required!', 'OK', { duration: 3000 });
       return;
     }
 
     this.userService.addUser(this.user).subscribe(
-      (data:any) => {
+      (data: any) => {
         console.log(data);
-        Swal.fire('Sucess','Username'+` `+data.username+` `+`is Registered`+`  `,'success');
+        Swal.fire('Success', `Username ${data.username} is Registered`, 'success').then(() => {
+          this.router.navigate(['/login']); // Redirect to login page
+        });
       },
       (error) => {
         console.log(error);
